7495000133d7c0402ebac0eedd1ec7c6933bc9c1
Alejandro Camacho Olivares <alejandro@uzu.digital>
2025-05-20 17:34:58 -0600

    Hero done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,9 +3,9 @@ import MobileTextHeroSection from "@assets/TextMobileHero.webp"
 
 export default function HeroSection() {
     return (
-        <section className="Hero">
-          <img className="relative top-0 max-[700px]:hidden" src={HeroTextMike.src} />
-          <img className="relative top-0 hidden max-[700px]:block" src={MobileTextHeroSection.src} />
+        <section className="bg-[url(/src/assets/HeroSectionMobile.webp)] sm:bg-[url(/src/assets/HeroMF.webp)] bg-fixed bg-cover">
+          <img className="relative top-0 mx-auto hidden sm:block" src={HeroTextMike.src} />
+          <img className="relative top-0 sm:hidden" src={MobileTextHeroSection.src} />
         </section>
     )
 }
