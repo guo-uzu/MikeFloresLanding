9727dc9e49a7cc67a344201044c2976fef14911d
Alejandro Camacho Olivares <alejandro@uzu.digital>
2025-05-20 16:43:10 -0600

    Hero done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import MobileTextHeroSection from "@assets/TextMobileHero.webp"
 
 export default function HeroSection() {
     return (
-        <section id="Hero">
+        <section className="Hero">
           <img className="relative top-0 max-[700px]:hidden" src={HeroTextMike.src} />
           <img className="relative top-0 hidden max-[700px]:block" src={MobileTextHeroSection.src} />
         </section>
