078e97dc3f617e8a2a39144e6f9ba560c24c2da9
Alejandro Camacho Olivares <alejandro@uzu.digital>
2025-05-21 10:54:43 -0600

    Hero fixed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,22 @@
 import HeroTextMike from "@assets/TextWebHero.webp"
 import MobileTextHeroSection from "@assets/TextMobileHero.webp"
+import { useEffect, useState } from "react"
 
 export default function HeroSection() {
-    return (
-        <section id="Hero">
-          <img className="relative top-0 max-[700px]:hidden" src={HeroTextMike.src} />
-          <img className="relative top-0 hidden max-[700px]:block" src={MobileTextHeroSection.src} />
-        </section>
-    )
+  const [activeClass, setActiveClass] = useState(false)
+
+  useEffect(() => {
+    const updateActiveClass = () => {
+      window.innerWidth >= 700 ? setActiveClass(true) : setActiveClass(false)
+    }
+    window.addEventListener("resize", updateActiveClass)
+    return () => window.removeEventListener("resize", updateActiveClass)
+  }, [])
+
+  return (
+    <section className={`${activeClass ? "hero-desktop" : "hero-mobile"}`}>
+      <img className="relative top-0 max-[700px]:hidden" src={HeroTextMike.src} />
+      <img className="relative top-0 hidden max-[700px]:block" src={MobileTextHeroSection.src} />
+    </section>
+  )
 }
